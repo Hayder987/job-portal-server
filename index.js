178e968bf9dd7a64b77393bff47d999e397f95fd
hayder <hayderbd4290@gmail.com>
2024-12-13 16:17:15 +0600

    appliction post api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const JobsCollection = client.db("Jobs-DB").collection("jobsCollection")
+    const applicationCollection = client.db("Jobs-DB").collection("applicationCollection")
 
     app.get('/alljobs', async(req, res)=>{
         const result = await JobsCollection.find().toArray()
@@ -40,10 +41,27 @@ async function run() {
       const {params}= req.query;
       let option = {}
       if(params){
-        option = {category:{$regex:params, $options:'i'}}
+        option = {
+          $or: [
+            { category: { $regex: params, $options: 'i' } },
+            { title: { $regex: params, $options: 'i' } }
+          ]
+        };
       }
-
       const result = await JobsCollection.find(option).toArray()
+      res.send(result)
+    })
+
+    app.get('/job', async(req, res)=>{
+      const {id} = req.query
+      const filter = { _id: new ObjectId(id)}
+      const result = await JobsCollection.findOne(filter)
+      res.send(result)
+    })
+
+    app.post('/application', async(req, res)=>{
+      const body = req.body;
+      const result = await applicationCollection.insertOne(body)
       res.send(result)
     })
 
